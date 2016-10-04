3745ada5c20c3cd76b4dae5f1f14bf8f2335d37f
Alberto Morgante Medina <alberto.morgante@bbva.com>
2016-10-04 18:01:50 +0200

    add new rabbit version

---
+++ rabbitmq/data/bootstrap.sh
@@ -0,0 +1,20 @@
+
+
+source functions.sh
+get_environment
+
+rabbitmq-server -detached
+sleep 2
+
+rabbitmqctl add_user $RABBIT_USERID $RABBIT_PASSWORD
+rabbitmqctl set_user_tags $RABBIT_USERID administrator 
+rabbitmqctl set_permissions -p / $RABBIT_USERID  ".*" ".*" ".*" 
+
+rabbitmqctl delete_user guest
+rabbitmqctl stop
+echo "*** User '$RABBITMQ_USER' with password '$RABBITMQ_PASSWORD' completed. ***"
+echo "*** Log in the WebUI at port 15672 ***"
+
+sleep 2
+ulimit -S -n 65536
+rabbitmq-server
