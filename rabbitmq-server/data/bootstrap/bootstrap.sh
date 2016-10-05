b06913ee045fdf676b1bac979c7bb939bb103ab8
tantru <ruben.fraile@bbva.com>
2016-10-05 11:26:23 +0200

    Update bootstrap.sh
    rabbitmq-server -detached
    stop 
    rabbitmq-server

--- rabbitmq-server/data/bootstrap/bootstrap.sh
+++ rabbitmq-server/data/bootstrap/bootstrap.sh
@@ -3,17 +3,20 @@
 source functions.sh
 get_environment
 
-ulimit -S -n 65536
-
-rabbitmq-server
-sleep 2
+rabbitmq-server -detached
+sleep 6
 
 rabbitmqctl add_user $RABBIT_USERID $RABBIT_PASSWORD
 rabbitmqctl set_user_tags $RABBIT_USERID administrator 
 rabbitmqctl set_permissions -p / $RABBIT_USERID  ".*" ".*" ".*" 
 
 rabbitmqctl delete_user guest
+rabbitmqctl stop
+sleep 4
 
 echo "*** User creation completed. ***"
 echo "*** Log in the WebUI at port 15672 ***"
+
+ulimit -S -n 65536
+rabbitmq-server
 
