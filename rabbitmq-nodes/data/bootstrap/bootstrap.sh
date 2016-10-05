658fdae19e4e2ba1703174ef20ce192e62a7a641
Alberto Morgante Medina <alberto.morgante@bbva.com>
2016-10-05 16:47:24 +0200

    add new version for jq

--- rabbitmq-nodes/data/bootstrap/bootstrap.sh
+++ rabbitmq-nodes/data/bootstrap/bootstrap.sh
@@ -4,13 +4,11 @@ source /bootstrap/functions.sh
 get_environment
 
 KUBE_TOKEN=$(</var/run/secrets/kubernetes.io/serviceaccount/token)
+var1=$(curl -sSk -H "Authorization: Bearer $KUBE_TOKEN" https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/api/v1/namespaces/default/pods/ | jq -c '.items[] | select(.status.containerStatuses[].name | contains("rmq"))' | jq '. | { name: .metadata.name, IP: .status.podIP}' | jq -r '[.IP, .name] | join(" ")')
 
-name_pod_raw=$(curl -sSk -H "Authorization: Bearer $KUBE_TOKEN" https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/api/v1/namespaces/default/pods/ | jq . | grep rmq-node-1- | grep name\" )
-name_pod=$(echo $name_pod_raw | awk -F"\"" '{print $4}' )
-ip_raw=$(curl -sSk -H "Authorization: Bearer $KUBE_TOKEN" https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/api/v1/namespaces/default/pods/$name_pod/ | jq . | grep podIP)
-ip_pod=$(echo $ip_raw | awk -F"\"" '{print $4}' )
-
-echo "$ip_pod $name_pod" >> /etc/hosts 
+for i in $var1; do
+	echo $i >> /etc/hosts 
+done
 
 echo $RABBIT_COOKIE > /var/lib/rabbitmq/.erlang.cookie 
 chown rabbitmq:rabbitmq /var/lib/rabbitmq/.erlang.cookie 
@@ -30,4 +28,3 @@ rabbitmq-server
 
 
 
-curl -sSk -H "Authorization: Bearer $KUBE_TOKEN" https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/api/v1/namespaces/default/pods/ | jq -c '.items[] | select(.status.containerStatuses[].name | contains("rmq"))' | jq '. | { name: .metadata.name, IP: .status.podIP}'
