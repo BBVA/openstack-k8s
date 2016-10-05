5eaa58e88f17001d045bafeebf1a33851c8a422e
Alberto Morgante Medina <alberto.morgante@bbva.com>
2016-10-05 17:18:06 +0200

    fix curl with  jq

--- rabbitmq-nodes/data/bootstrap/bootstrap.sh
+++ rabbitmq-nodes/data/bootstrap/bootstrap.sh
@@ -6,7 +6,7 @@ get_environment
 KUBE_TOKEN=$(</var/run/secrets/kubernetes.io/serviceaccount/token)
 curl -sSk -H "Authorization: Bearer $KUBE_TOKEN" https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/api/v1/namespaces/default/pods/ | jq -c '.items[] | select(.status.containerStatuses[].name | contains("rmq"))' | jq '. | { name: .metadata.name, IP: .status.podIP}' | jq -r '[.IP, .name] | join(" ")' >> /etc/hosts
 
-name_pod=curl -sSk -H "Authorization: Bearer $KUBE_TOKEN" https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/api/v1/namespaces/default/pods/ | jq -c '.items[] | select(.metadata.name | contains("rmq-node-1"))' | jq '. | { name: .metadata.name }' | jq -r .[]
+name_pod=$(curl -sSk -H "Authorization: Bearer $KUBE_TOKEN" https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/api/v1/namespaces/default/pods/ | jq -c '.items[] | select(.metadata.name | contains("rmq-node-1"))' | jq '. | { name: .metadata.name }' | jq -r .[])
 
 echo $RABBIT_COOKIE > /var/lib/rabbitmq/.erlang.cookie 
 chown rabbitmq:rabbitmq /var/lib/rabbitmq/.erlang.cookie 
