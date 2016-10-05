18c334dcb0414f06d860b17256dca07c8c7b45cf
Alberto Morgante Medina <alberto.morgante@bbva.com>
2016-10-05 13:19:57 +0200

    get the ip and name from k8s metadata

--- rabbitmq-nodes/data/bootstrap/bootstrap.sh
+++ rabbitmq-nodes/data/bootstrap/bootstrap.sh
@@ -3,16 +3,25 @@
 source /bootstrap/functions.sh
 get_environment
 
+KUBE_TOKEN=$(</var/run/secrets/kubernetes.io/serviceaccount/token)
+
+name_pod_raw=$(curl -sSk -H "Authorization: Bearer $KUBE_TOKEN" https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/api/v1/namespaces/default/pods/ | jq . | grep rmq-node-1- | grep name\" )
+name_pod=$(echo $name_pod_raw | awk -F"\"" '{print $4}' )
+ip_raw=$(curl -sSk -H "Authorization: Bearer $KUBE_TOKEN" https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/api/v1/namespaces/default/pods/$name_pod/ | jq . | grep podIP)
+ip_pod=$(echo $ip_raw | awk -F"\"" '{print $4}' )
+
+echo "$ip_pod $name_pod" >> /etc/hosts 
+
 echo $RABBIT_COOKIE > /var/lib/rabbitmq/.erlang.cookie 
 chown rabbitmq:rabbitmq /var/lib/rabbitmq/.erlang.cookie 
 chmod 400 /var/lib/rabbitmq/.erlang.cookie 
 
 rabbitmq-server -detached 
 rabbitmqctl stop_app
-rabbitmqctl join_cluster rabbit@rmq-node-1-svc.default.svc
+rabbitmqctl join_cluster rabbit@$name_pod
 rabbitmqctl start_app
 
 rabbitmqctl stop
-sleep 2
+sleep 4
 rabbitmq-server 
 
