4be736321ae5291399687ae8edc7c8dbd821770a
Fernando Alvarez <methadato@gmail.com>
2016-10-10 18:34:51 +0200

    Use container's secondary IP in VSC config.cfg

--- vsc/data/bootstrap/bootstrap.sh
+++ vsc/data/bootstrap/bootstrap.sh
@@ -1,11 +1,65 @@
 #!/usr/bin/env bash
 
+# atoi: Returns the integer representation of an IP arg, passed in ascii
+# dotted-decimal notation (x.x.x.x)
+atoi() {
+  local IP=$1
+  local IPnum=0
+  for (( i=0 ; i<4 ; ++i ))
+  do
+    ((IPnum+=${IP%%.*}*$((256**$((3-${i}))))))
+    IP=${IP#*.}
+  done
+  echo $IPnum
+}
+
+# itoa: returns the dotted-decimal ascii form of an IP arg passed in integer
+# format
+itoa() {
+  echo -n $(($(($(($((${1}/256))/256))/256))%256)).
+  echo -n $(($(($((${1}/256))/256))%256)).
+  echo -n $(($((${1}/256))%256)).
+  echo $((${1}%256))
+}
 
 source /bootstrap/functions.sh
 get_environment
 
-# Get my IPv4 address (including netmask)
-read _ MY_IP _  < <(ip addr show eth0 | grep "inet ")
+getIP() {
+
+  local_ifaces=($(ip link show | grep -v noop | grep state | grep -v LOOPBACK | awk '{print $2}' | tr -d : | sed 's/@.*$//'))
+  local IP MASK
+  for iface in "${local_ifaces[@]}"; do
+    # Get my IPv4 address (including netmask)
+    local IPs=$(ip address show dev $iface | grep inet | awk '/inet / { print $2 }' | cut -f1 -d/)
+    IPs=($IPs)
+
+    if ! [[ -z "$SELECTED_NETWORK" ]]; then
+      local given_ip given_mask
+      IFS=/ read given_ip given_mask <<< $SELECTED_NETWORK
+      local given_addr=$(atoi $given_ip)
+      local given_cidr=$given_mask
+      local given_mask=$((0xffffffff << (32 - $given_mask) & 0xffffffff))
+      local given_broadcast=$((given_addr | ~given_mask & 0xffffffff))
+      local given_network=$((given_addr & given_mask))
+
+      for configured_ip in "${IPs[@]}"; do
+        local configured_ip=$(atoi $configured_ip)
+        if [[ $configured_ip -gt $given_network && $configured_ip -lt $given_broadcast ]]; then
+          IP=$(itoa $configured_ip)
+          CIDR=$given_cidr
+        fi
+      done
+      [[ -z "$IP" ]] && echo "WARNING: SELECTED_NETWORK ($SELECTED_NETWORK) not found in $iface interface."
+    else
+      IP=${IPs[0]}
+    fi
+  done
+  echo "$IP" "$CIDR"
+}
+
+read MY_IP MY_CIDR < <(getIP)
+echo $MY_IP / $MY_CIDR
 
 # Get the local GW
 read _ _ MY_GW _ < <(ip route list match 0/0)
@@ -15,6 +69,7 @@ read _ MY_DNS  < <(cat /etc/resolv.conf | grep nameserver)
 
 MY_HOSTNAME=`hostname`
 
+export MY_CIDR
 export MY_IP
 export MY_GW
 export MY_DNS
