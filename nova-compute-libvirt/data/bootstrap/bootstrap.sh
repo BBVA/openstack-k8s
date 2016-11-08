be8f425fa3736366e2735c960b3fd7cbf77eb8de
Alberto Morgante Medina <alberto.morgante@bbva.com>
2016-11-08 16:56:24 +0100

    add libvirt config in nova.conf and libvirt.conf

--- nova-compute-libvirt/data/bootstrap/bootstrap.sh
+++ nova-compute-libvirt/data/bootstrap/bootstrap.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
-
+cat > /etc/libvirt/libvirt.conf <<EOF
+listen_tls = 0
+listen_tcp = 1
+auth_tcp="none"
+tcp_port = "16509"
+listen_addr = "0.0.0.0"
+EOF
 
 mkdir /var/run/dbus/
 mkdir /usr/local/lib/python2.7/dist-packages/instances
@@ -10,5 +16,5 @@ chown root:kvm /dev/kvm
 chmod 666 /dev/kvm
 sleep 4
 dbus-daemon --config-file=/etc/dbus-1/system.conf &
-libvirtd &
+libvirtd -d -l -f /etc/libvirt/libvirt.conf &
 sleep 1d
